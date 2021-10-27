791fe5e91b347f95e15ee64ed76126b27b666452
Welves de Paula <welvesdepaula@gmail.com>
2021-10-27 20:31:22 -0300

    dev

---
+++ src/mixins/chat/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+var config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+};
+
+var fire = firebase.initializeApp(config);
+export default fire;
