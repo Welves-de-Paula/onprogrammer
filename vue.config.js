807c446989e14135c7aae680e65348a1f0b462d8
Welves de Paula <welvesdepaula@gmail.com>
2021-11-05 21:50:23 -0300

    dev

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,15 @@ module.exports = {
     'vuetify'
   ],
 
+
+  // output built static files to Laravel's public dir.
+  // note the "build" script in package.json needs to be modified as well.
+  // outputDir: '../backend/public',
+
+  // modify the location of the generated HTML file.
+  // make sure to do this only in production.
+
+  // indexPath: process.env.NODE_ENV === 'production'
+  //   ? './backend/backend/public/index.html'
+  //   : 'index.html'
 }
