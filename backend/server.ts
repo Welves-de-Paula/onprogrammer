f132ec8b414954596157b1c531846401c1f76902
Welves de Paula <welvesdepaula@gmail.com>
2021-11-09 21:36:58 -0300

    dev

--- backend/server.ts
+++
@@ -1,6 +0,0 @@
-import { http } from "./http";
-import "./websocket/client";
-import "./websocket/admin";
-const dotenv = require ('dotenv'); 
-dotenv.config (); 
- http.listen(process.env.BACKEND_PORT, () => console.log(`Sua porta é ${process.env.BACKEND_PORT}`));
