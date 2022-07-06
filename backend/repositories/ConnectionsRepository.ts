a522004a0ccdf9c3ce45aff23cec5783c47db790
welves de paula <welvesdepaula@gmail.com>
2022-07-06 07:06:47 -0300

    dev update

--- backend/repositories/ConnectionsRepository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-
-import { Connection } from "../entities/Connection";
-
-@EntityRepository(Connection)
-class ConnectionsRepository extends Repository<Connection> {}
-
-export { ConnectionsRepository };
--- backend/server.ts
+++
@@ -1,5 +0,0 @@
-import { http } from "./config/app";
-import "./websocket/client";
-import "./websocket/admin";
-require('dotenv').config();
-http.listen(process.env.BACKEND_PORT, () => console.log(`O servidor está rodando na porta  ${process.env.BACKEND_PORT} 🚀`));
